8da5144470afa15650205cab2a7926a0b9303432
Sravanichain <id2sravani@gmail.com>
2024-12-18 18:52:23 -0600

    Add files via upload

--- script/Books.s.sol
+++ script/Books.s.sol
@@ -1,14 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 
-import {Script} from "forge-std/Script.sol";
-import {Books} from "../src/Books.sol";
+import {Script, console} from "forge-std/Script.sol";
+import "../src/Books.sol";
 
 contract DeployBooks is Script {
-    Books public books;
+    Books public midterm;
+    Books.Book public book;
 
+    function setUp() public {
+        vm.broadcast();
+
+        midterm = new Books("Programming Foundry", "Ellan", 100);
+    }
 
     function run() public {
-        books = new Books("Programming Foundry", "Sravani", 100);
+        log_book();
+    }
+
+    function log_book() internal{
+        book = midterm.get_book();
+        console.log("Author :", book.author);
     }
 }
--- test/Books.t.sol
+++ test/Books.t.sol
@@ -1,32 +1,34 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import "../src/Books.sol";
 
-contract TestBooks is Test {
-    Books public books;
+contract TestBooks is Test{
+
+    Books midterm;
     Books.Book public book;
 
+
     function setUp() public {
-        books = new Books("Programming Foundry", "Sravani", 100);
-        console.log("Books contract deployed at address:", address(books));
+        midterm = new Books("Programming Foundry", "Ellan", 100);
+        console.log("Midterm contract Deployed at:",address(midterm));
     }
 
-    function test_true() public pure {
+    function test_true() public pure{
         assert(true);
     }
 
-    function test_get_book() public {
-        book = books.get_book();
-        assertEq(book.title, "Programming Foundry");
-        assertEq(book.author, "Sravani");
-        assertEq(book.pages, 100);
+    function test_get_book() public{
+        book=midterm.get_book();
+        assertEq(book.pages,100);
+        assertEq(book.author,"Ellan");
+        assertEq(book.title,"Programming Foundry");
     }
 
-    function test_update_pages() public {
-        books.update_pages(200);
-        book = books.get_book();
-        assertEq(book.pages, 200);
+    function test_update_pages() public{
+        midterm.update_pages(200);
+        book= midterm.get_book();
+        assertEq(book.pages,200);
     }
 }
